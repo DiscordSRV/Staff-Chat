670d2c34e03eb180e1dbc375ab1d52a803a89a60
RezzedUp <RezzedUp@users.noreply.github.com>
2019-04-04 09:51:50 -0500

    Update plugin.yml. Optimize imports.

--- src/main/java/com/rezzedup/discordsrv/staffchat/Permissions.java
+++ src/main/java/com/rezzedup/discordsrv/staffchat/Permissions.java
@@ -2,9 +2,6 @@ package com.rezzedup.discordsrv.staffchat;
 
 import org.bukkit.permissions.Permissible;
 
-import java.util.Arrays;
-import java.util.function.Predicate;
-
 public enum Permissions
 {
     ALL("*"),
