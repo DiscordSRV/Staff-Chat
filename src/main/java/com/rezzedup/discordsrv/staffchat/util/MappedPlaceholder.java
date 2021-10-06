d949c0b47012770826d0638fb5d8595e1ae6783f
RezzedUp <RezzedUp@users.noreply.github.com>
2021-10-05 23:34:37 -0500

    Improve placeholders implementation.

--- src/main/java/com/rezzedup/discordsrv/staffchat/util/MappedPlaceholder.java
+++ src/main/java/com/rezzedup/discordsrv/staffchat/util/MappedPlaceholder.java
@@ -24,31 +24,27 @@ package com.rezzedup.discordsrv.staffchat.util;
 
 import pl.tlinkowski.annotation.basic.NullOr;
 
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.Objects;
 import java.util.function.Supplier;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class MappedPlaceholder
 {
-    public static Pattern PATTERN = Pattern.compile("%.+?%");
+    public static Pattern PATTERN = Pattern.compile("%(.+?)%");
     
     protected final Map<String, Supplier<?>> placeholders = new HashMap<>();
     
-    public String get(String placeholder)
+    public String get(@NullOr String placeholder)
     {
         if (Strings.isEmptyOrNull(placeholder)) { return ""; }
         
-        Supplier<?> supplier = placeholders.get(placeholder.toLowerCase());
-        
+        Supplier<?> supplier = placeholders.get(placeholder.toLowerCase(Locale.ROOT));
         if (supplier == null) { return ""; }
         
         @NullOr Object result = null;
-        
         try { result = supplier.get(); }
         catch (Exception e) { e.printStackTrace(); }
         
@@ -57,35 +53,26 @@ public class MappedPlaceholder
     
     public String update(String message)
     {
-        Matcher matcher = PATTERN.matcher(message);
-        
-        while (matcher.find())
-        {
-            String match = matcher.group();
-            String value = get(match.replace("%", ""));
-            
-            if (Strings.isEmptyOrNull(value)) { continue; }
-            
-            message = message.replace(match, value);
-        }
-        
-        return message;
+        return PATTERN.matcher(message).replaceAll(mr -> {
+            String value = get(mr.group(1));
+            return (value.isEmpty()) ? mr.group() : value;
+        });
     }
     
     public Putter map(String ... placeholders)
     {
         Objects.requireNonNull(placeholders, "placeholders");
         if (placeholders.length <= 0) { throw new IllegalArgumentException("Empty placeholders array"); }
-        return new Putter(Arrays.asList(placeholders));
+        return new Putter(placeholders);
     }
     
     public void inherit(MappedPlaceholder from) { placeholders.putAll(from.placeholders); }
     
     public class Putter
     {
-        private final List<String> aliases;
+        private final String[] aliases;
         
-        private Putter(List<String> aliases) { this.aliases = aliases; }
+        private Putter(String[] aliases) { this.aliases = aliases; }
         
         public void to(Supplier<?> supplier)
         {
@@ -94,9 +81,7 @@ public class MappedPlaceholder
             for (String alias : aliases)
             {
                 if (Strings.isEmptyOrNull(alias)) { continue; }
-                
-                String placeholder = alias.toLowerCase();
-                MappedPlaceholder.this.placeholders.put(placeholder, supplier);
+                placeholders.put(alias.toLowerCase(Locale.ROOT), supplier);
             }
         }
     }
