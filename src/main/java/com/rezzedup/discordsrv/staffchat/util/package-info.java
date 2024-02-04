6e50aeccca818fac1ebd5a7eb90094f168452213
RezzedUp <RezzedUp@users.noreply.github.com>
2024-02-03 19:51:05 -0600

    Update dependencies and license headers.

--- src/main/java/com/rezzedup/discordsrv/staffchat/util/package-info.java
+++ src/main/java/com/rezzedup/discordsrv/staffchat/util/package-info.java
@@ -1,6 +1,6 @@
 /*
  * The MIT License
- * Copyright © 2017-2022 RezzedUp and Contributors
+ * Copyright © 2017-2024 RezzedUp and Contributors
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
